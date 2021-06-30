075ef843f5c9051e94f793d28c87a8660be92e8c
root <root@SDPRJS-GPU-linux1.ph4zyqij0zwe5bkahprsl3mlqe.bx.internal.cloudapp.net>
2021-06-30 13:20:21 +0000

    logstash 추가

---
+++ Dockerfiles/flaskdocker/dash_app2/final_project/c_crawling.py
@@ -0,0 +1,103 @@
+from pytchat import LiveChat
+import pafy
+
+import socket
+import re
+from datetime import datetime as dt
+
+import c_kafka
+import c_token
+
+import random # 삭제 필요
+
+ck = c_kafka.c_kafka()
+
+class c_crawling():
+    def youtube_to_kafka(self, url_id, topic):
+        pafy.set_api_key('AIzaSyB0oN8aGAJDk5PEWpR07Tn7gN4w30fwnOI')
+
+        num = 0
+        chat = LiveChat(video_id=url_id, topchat_only='FALSE', interruptable=False)
+        while chat.is_alive():
+            try:
+                data = chat.get()
+                items = data.items
+                for c in items:
+                    d = self.c_json(url_id, c.author.name, num, c.message, c.datetime)
+                    num += 1
+                    ck.To_kafka(topic, d)
+            except KeyboardInterrupt:
+                chat.terminate()
+                break
+
+    def twitch_to_kafka(self, url_id, topic):
+        sock = socket.socket()
+
+        count = 0
+        num = 0
+
+        port = 6667
+        server = 'irc.chat.twitch.tv'
+        sock.connect((server, port))
+
+        token = 'oauth:snc65htpml9l2qxx3qfb75qqqe1swm'
+        nickname = 'deokki9880'
+        channel = "#"+url_id
+
+        sock.send(f"PASS {token}\n".encode('utf-8'))
+        sock.send(f"NICK {nickname}\n".encode('utf-8'))
+        sock.send(f"JOIN {channel}\n".encode('utf-8'))
+
+        resp = sock.recv(4096).decode('utf-8', 'ignore')
+        while True:
+            c_time = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+            minute = int(c_time.split(':')[1])
+            if minute % 10 == 0:
+                if count == 0:
+                    print('\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\')
+                    count+=1
+                else:
+                    resp = sock.recv(4096).decode('utf-8','ignore')
+            else:
+                resp = sock.recv(4096).decode('utf-8','ignore')
+                count = 0
+
+            test = resp.split('\n')
+
+            if '' in test:
+                test.remove('')
+            for i in test:
+                temp = re.search('@[^\r]+\r', i)
+                if temp == None:
+                    continue
+                elif temp.group == '':
+                    continue
+                else:
+                    text_list = re.findall("\ :[^\r]+\r", i)
+                    id_list = re.findall('@[^.]+.', i)
+                    if len(text_list) == 0 or len(id_list) == 0:
+                        continue
+                    text_list = text_list[0].replace(' :', '').rstrip()
+                    id_list = id_list[0].replace('@', '').replace('.', '')
+                d = self.c_json(url_id, id_list, num, text_list, c_time)
+                ck.To_kafka(topic, d)
+                num += 1
+
+    def c_json(self, video_id, user_name, chat_id, chat_text, chat_time):
+        ct = c_token.c_token()
+
+        tmp_json = {
+            "video_id": video_id,
+            "user_name": user_name,
+            "chat_id": chat_id,
+            "chat_text": chat_text,
+            "modified_text": ct.only_korean(chat_text),
+            "h_token": ct.m_tokenize(chat_text),
+            "j_token": ct.c_tokenize(chat_text),
+            "hj_token": ct.cm_tokenize(chat_text),
+            "noun_token": ct.noun_tokenize(chat_text),
+            "chat_time": chat_time,
+            "emotion": round(random.random(), 1), # 기능 테스트옹 삭제 필요
+            "abuse": round(random.random(), 1) # 기능 테스트용 삭제 필요
+        }
+        return tmp_json
---
+++ Dockerfiles/flaskdocker/dash_app2/final_project/chatbot.py
@@ -0,0 +1,108 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import flash
+from flask import jsonify
+
+import pafy
+import multiprocessing as mp
+import os
+from glob import glob
+
+import c_crawling
+import c_kafka
+import c_wordcloud
+import time
+
+app = Flask(__name__, static_url_path='/static')
+app.secret_key = 'admin'
+kafka_topic = "test_topic"
+
+cc = c_crawling.c_crawling()
+ck = c_kafka.c_kafka()
+cwc = c_wordcloud.c_wordcloud()
+
+@app.route('/')
+def main_chatbot():
+    if mp.active_children():
+        for i in mp.active_children():
+            i.terminate()
+    time.sleep(0.1)
+    [os.remove(f) for f in glob("./static/img/*.png")]
+    global wordcloud_lst
+    wordcloud_lst = ['시작']
+    return render_template('index.html')
+
+@app.route('/chatcategory', methods=['POST'])
+def chat_category():
+    category_val = request.form.get('categories')
+    url_val = request.form.get('searchbar')
+    if url_val:
+        if category_val == 'youtube':
+            try:
+                url_id = url_val.split("=")[1] # 주소 형태 : https://www.youtube.com/watch?v=MN1JsMbNMs0
+                p = mp.Process(target=cc.youtube_to_kafka, args=(url_id, kafka_topic,))
+                p.start()
+                pafy.set_api_key('AIzaSyB0oN8aGAJDk5PEWpR07Tn7gN4w30fwnOI')
+                v = pafy.new(url_id)
+                return render_template('result.html', data_list=v.title)
+            except:
+                flash('Youtube URL을 입력해주세요')
+                return render_template('index.html')
+        elif category_val == 'twitch':
+            url_id = url_val.split("/")[-1] # 주소 형태: https://www.twitch.tv/lol_ambition
+            if '=' in url_id:
+                flash('Twitch URL을 입력해주세요')
+                return render_template('index.html')
+            else:
+                p = mp.Process(target=cc.twitch_to_kafka, args=(url_id, kafka_topic,))
+                p.start()
+                return render_template('result.html', data_list=url_id+'의 방송입니다')
+        elif category_val == 'afreecatv':
+            return render_template('result.html', data_list='아직 구현되지 않은 기능입니다')
+    else:
+        flash('URL을 입력해주세요')
+        return render_template('index.html')
+
+num = 1
+wordcloud_lst = ['시작']
+@app.route('/update', methods=['POST'])
+def update():
+    global num
+    consumer = ck.From_kafka(kafka_topic)
+    for message in consumer:
+        print({"msg": message.value})
+        if message.value['noun_token']:
+            wordcloud_lst.extend(message.value['noun_token'])
+        if len(wordcloud_lst) > 200:
+            del wordcloud_lst[:len(wordcloud_lst) - 200]
+        cwc.woco_pic(wordcloud_lst, num, message.value['video_id'])
+        num += 1
+        image_name = message.value['video_id']+"_"+str(num-5).zfill(4)+".png"
+        print(len(wordcloud_lst))
+        return jsonify({"msg":message.value, "image_name": image_name})
+
+import logging
+import datetime
+from pytz import timezone
+
+logging.basicConfig(filename="logs/chatbot.log", level=logging.DEBUG)
+
+def log(request, message):
+    log_date = get_log_date()
+    log_message = "{0}/{1}/{2}".format(log_date, str(request), message)
+    logging.info(log_message)
+
+def error_log(request, error_code, error_message):
+    log_date = get_log_date()
+    log_message = "{0}/{1}/{2}/{3}".format(log_date, str(request), error_code, error_message)
+    logging.info(log_message)
+
+def get_log_date():
+    dt = datetime.datetime.now(timezone("Asia/Seoul"))
+    log_date = dt.strftime("%Y%m%d_%H:%M:%S")
+    return log_date
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, use_reloader=False)
+
