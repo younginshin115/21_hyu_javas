39830af8df7acdeeb4207768fa4a5e7c887460de
Jang-Ji-Yeon <delayeon9934@naver.com>
2021-07-02 17:34:11 +0900

    0701_update emotion0628.py

--- pipeline/emotion.py
+++ pipeline/emotion.py
@@ -1,28 +1,36 @@
+import re
+import os
 import json
 from konlpy.tag import Okt
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.layers import Embedding, Dense, LSTM
-from tensorflow.keras.models import Sequential
 from tensorflow.keras.models import load_model
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
 
-loaded_model = load_model('/home/adminuser/mymodel/sentiment_model_0.1.h5')
+loaded_model = load_model('/home/adminuser/mymodel/slang_model_0.1.h5') 
 
-okt = Okt()
+okt = Okt() 
 stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다'] 
 max_len = 30 
-tokenizer = Tokenizer()
-with open('/home/adminuser/mymodel/sentiment_vocab_0.1.json') as json_file:
+vocab_size = 4346 
+tokenizer = Tokenizer(vocab_size) 
+with open('/home/adminuser/mymodel/slang_vocab_0.1.json', encoding='UTF8') as json_file:
     vocab = json.load(json_file)
     tokenizer.word_index = vocab
 
-def sentiment_predict(new_sentence):
-    new_sentence = [word for word in new_sentence if not word in stopwords]
-    #  tokenizer.fit_on_texts(new_sentence)
-    encoded = tokenizer.texts_to_sequences([new_sentence])
-    pad_new = pad_sequences(encoded, maxlen = max_len)
-    score = float(loaded_model.predict(pad_new))
-    return round(score,1)
+
+def slang_predict(new_sentence):
+    new_sentence = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣 ]', '', new_sentence)
+
+    if new_sentence == "" or new_sentence.isspace():    
+        return float(-1)  
+    else :                                           
+        new_sentence = okt.morphs(new_sentence, stem=True) 
+        new_sentence = [word for word in new_sentence if not word in stopwords] 
+    if not new_sentence : 
+        return float(-1)
+    else:
+        encoded = tokenizer.texts_to_sequences([new_sentence])
+        pad_new = pad_sequences(encoded, maxlen = max_len)
+        score = loaded_model.predict(pad_new)
+        return round(float(score), 2)
